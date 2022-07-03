c1eed2b3b4a51b65d7ed1f517d8b13e2e19958d8
Tomas Regalado Manay <andersonedlp_10@hotmail.com>
2022-07-03 18:25:54 -0300

    nav completamente responsive

---
+++ js/script.js
@@ -0,0 +1,10 @@
+addEventListener('DOMContentLoaded', () => {
+    const btnMenu = document.getElementById('btn');
+
+    if (btnMenu) {
+        btnMenu.addEventListener('click', () => {
+            const menuOp = document.getElementById('menuOp');
+            menuOp.classList.toggle('show');
+        })
+    }
+})

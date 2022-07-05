1a08cc2ba1307115734d8505eff72c13ebba186b
Tomas Regalado Manay <andersonedlp_10@hotmail.com>
2022-07-05 19:27:53 -0300

    agregue camisetas

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ addEventListener('DOMContentLoaded', () => {
 function infoCreador() {
     alert('Pagina Web desarrollado por Tomas Regalado Manay');
 }
+
+function showGracias1() {
+    const boton1 = document.getElementById('boton1');
+    boton1.innerHTML = "¡ GRACIAS POR SU COMPRA !";
+}
+
+function showGracias2() {
+    const boton2 = document.getElementById('boton2');
+    boton2.innerHTML = "¡ GRACIAS POR SU COMPRA !";
+}
+
+function showGracias3() {
+    const boton3 = document.getElementById('boton3');
+    boton3.innerHTML = "¡ GRACIAS POR SU COMPRA !";
+}
